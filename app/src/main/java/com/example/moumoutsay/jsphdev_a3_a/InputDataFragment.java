f2d4422ad99b3a1c5342a25d24dbb69f141834f9
moumoutsay <weilints@andrew.cmu.edu>
2015-04-06 16:47:39 -0700

    input data and output stat layout ready

---
+++ app/src/main/java/com/example/moumoutsay/jsphdev_a3_a/InputDataFragment.java
@@ -0,0 +1,23 @@
+package com.example.moumoutsay.jsphdev_a3_a;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by moumoutsay on 4/6/15.
+ */
+public class InputDataFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_input_data, container, false);
+        return rootView;
+    }
+}
